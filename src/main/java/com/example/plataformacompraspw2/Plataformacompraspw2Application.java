fb3be7663b0352a1673e77b174897e1f68e617fa
bernard.carvalho <bernard.carvalho@ifto.edu.br>
2022-11-04 17:56:51 -0300

    Initial commit

---
+++ src/main/java/com/example/plataformacompraspw2/Plataformacompraspw2Application.java
@@ -0,0 +1,13 @@
+package com.example.plataformacompraspw2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Plataformacompraspw2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Plataformacompraspw2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/plataformacompraspw2/Plataformacompraspw2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.plataformacompraspw2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Plataformacompraspw2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
